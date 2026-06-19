83cc4553d35165f6df8eade471e9ed16df25cc7a
ohiweme <ohiwemeh@gmail.com>
2026-06-19 06:50:58 +0100

    changes

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express(); // ✅ Must come BEFORE app.use()
 
 // Middlewares
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://gcasl-philippines.vercel.app','https://www.bdotrustfundspayoutgateway.com','https://bdotrustfundspayoutgateway.vercel.app'],
+  origin: ['http://localhost:5173','https://www.bdotrustfundspayoutgateway.com','https://bdotrustfundspayoutgateway.vercel.app'],
   credentials: true,
 }));
 
